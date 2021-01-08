6e559db930856dea90455233cd2c3458671aaa9c
visheshshukla <engr.visheshshukla@gmail.com>
2021-01-08 22:06:59 +0530

    updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from 'react';
+import App, { Container } from 'next/app';
+
+// Stylings
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/main.scss';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
 }
 
 export default MyApp
